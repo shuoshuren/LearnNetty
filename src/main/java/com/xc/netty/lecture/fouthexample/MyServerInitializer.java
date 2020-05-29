cdcae7eeba77f75a5a02685273aa1a1a338b2a67
shuoshuren <534361124@qq.com>
2020-05-30 00:18:04 +0800

    服务器心跳检测

---
+++ src/main/java/com/xc/netty/lecture/fouthexample/MyServerInitializer.java
@@ -0,0 +1,24 @@
+package com.xc.netty.lecture.fouthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
+import io.netty.util.CharsetUtil;
+
+import java.util.concurrent.TimeUnit;
+
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new IdleStateHandler(3,5,7, TimeUnit.SECONDS));
+        pipeline.addLast(new MyServerHandler());
+    }
+}
